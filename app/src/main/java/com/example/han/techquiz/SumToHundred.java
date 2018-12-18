becd505affcc5260e18b8b840e2ff14a4b5b84b6
Han <et801102@gmail.com>
2018-12-19 01:04:08 +0800

    Implement Q1

---
+++ app/src/main/java/com/example/han/techquiz/SumToHundred.java
@@ -0,0 +1,69 @@
+package com.example.han.techquiz;
+
+public class SumToHundred {
+    // first one only have one state
+    private static final int DIGITS = 9;
+    int[] state = new int[DIGITS - 1];
+
+    class State {
+        private static final int PLUS = 0;
+        private static final int MINUS = 1;
+        private static final int CARRY = 2;
+    }
+
+    public void start() {
+        int carryPointer;
+        while (state[DIGITS - 2] <= 2) {
+            countAns();
+            carryPointer = 0;
+            state[carryPointer]++;
+            while (state[carryPointer] > 2 && state[DIGITS - 2] <= 2) {
+                state[carryPointer] = 0;
+                state[++carryPointer]++;
+            }
+        }
+    }
+
+    private void countAns() {
+        int prevNum = 1;
+        int ans = 0;
+        StringBuilder formula = new StringBuilder();
+        for (int i = 0; i < 8; i++) {
+            switch (state[i]) {
+                case State.PLUS:
+                    appendNumber(formula, prevNum);
+                    ans += prevNum;
+                    prevNum = i + 2;
+                    break;
+                case State.MINUS:
+                    appendNumber(formula, prevNum);
+                    ans += prevNum;
+                    prevNum = -(i + 2);
+                    break;
+                case State.CARRY:
+                    if (prevNum > 0) {
+                        prevNum = prevNum * 10 + i + 2;
+                    } else {
+                        prevNum = prevNum * 10 - (i + 2);
+                    }
+
+                    break;
+            }
+        }
+        appendNumber(formula, prevNum);
+        ans += prevNum;
+        if (ans == 100) {
+            System.out.println(formula.toString());
+        }
+    }
+
+    private void appendNumber(StringBuilder stringBuilder, int number) {
+        if (stringBuilder.toString().equals("")) {
+            stringBuilder.append(number);
+        } else if (number > 0) {
+            stringBuilder.append(" + ").append(number);
+        } else {
+            stringBuilder.append(" - ").append(Math.abs(number));
+        }
+    }
+}
